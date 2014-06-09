aa3c1d705545de8595373d3202a4b6417dd08e3d
Jamie Holdroyd <jamie@molt.in>
2014-06-09 14:08:16 +0100

    Updating examples

--- examples/assets/examples.js
+++ examples/assets/examples.js
@@ -1,11 +1,14 @@
 $(function() {
+
+	// Helpers
+	Object.prototype.firstItem = function() { return this[Object.keys(this)[0]]; }
 	
 	// Get example code
 	var str = $('script[type="text/javascript"]').html();
 	str     = str.replace("({publicId: '85e4IP87t5jlB5w2Lv366UX0pJB69D2A0g309SSk'})", "({publicId: '<YOUR PUBLIC ID>'})");
-	str     = str.replace("           $('#sample').html(JSON.stringify(product, null, '\\t'));\n\n", '');
+	str     = str.replace("      $('#sample').html(JSON.stringify(response, null, '\\t'));\n\n", '');
 
 	// Implement code
-	$('#code').text(str);
+	$('#code').text(str.trim());
 
 });
