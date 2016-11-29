a78037c967421315ec907020cad2d81b4dcd75fc
Jonathan Steele ⚡️ <jonathan@steele.pro>
2016-11-29 22:11:47 +0000

    :bug: Updated payloads (#15)

--- dist/moltin.js
+++ dist/moltin.js
@@ -39,21 +39,22 @@ var Moltin = function () {
     this.RequestFactory = new RequestFactory(this);
 
     this.Products = new Products(this);
+    this.Cart = new Cart(this);
+    this.Orders = new Orders(this);
+    this.Categories = new Categories(this);
+    this.Currency = new Currency(this);
+    this.Brands = new Brands(this);
+    this.Collections = new Collections(this);
 
     /*
     @Shipping      = new Shipping @
     @Tax           = new Tax @
     @Address       = new Address @
     @Brand         = new Brand @
-    @Cart          = new Cart @
-    @Category      = new Category @
     @Checkout      = new Checkout @
-    @Collection    = new Collection @
-    @Currency      = new Currency @
     @Entry         = new Entry @
     @Gateway       = new Gateway @
     @Language      = new Language @
-    @Order         = new Order @
     */
 
     if (this.Storage.get('mcurrency')) {
@@ -112,8 +113,15 @@ var Moltin = function () {
       var promise = new Promise(function (resolve, reject) {
         var req = function req() {
           var token = s.get('mtoken');
-          headers['Authorization'] = 'Bearer: ' + token;
-          headers['Content-Type'] = t.config.contentType;
+
+          headers = {
+            'Authorization': 'Bearer: ' + token,
+            'Content-Type': t.config.contentType
+          };
+
+          if (t.config.currency) {
+            headers['X-MOLTIN-CURRENCY'] = t.config.currency;
+          }
 
           return r.make(uri, method, data, headers).then(function (data) {
             return resolve(data);
@@ -147,8 +155,18 @@ var Abstract = function () {
 
   _createClass(Abstract, [{
     key: 'Get',
-    value: function Get(id, callback, error) {
-      return this.m.Request(this.endpoint + '/' + id, 'GET', null, callback, error);
+    value: function Get(id, params) {
+      if (this.endpoint === 'carts') {
+        return this.m.Request(this.endpoint + '/' + this.cartId, 'GET');
+      }
+
+      if (params) {
+        var includes = params.toString();
+
+        return this.m.Request(this.endpoint + '/' + id + '?include=' + includes, 'GET');
+      }
+
+      return this.m.Request(this.endpoint + '/' + id, 'GET');
     }
   }, {
     key: 'Find',
@@ -157,8 +175,14 @@ var Abstract = function () {
     }
   }, {
     key: 'List',
-    value: function List(terms) {
-      return this.m.Request(this.endpoint, 'GET', terms);
+    value: function List(params) {
+      if (params) {
+        var includes = params.toString();
+
+        return this.m.Request(this.endpoint + '?include=' + includes, 'GET');
+      }
+
+      return this.m.Request(this.endpoint, 'GET');
     }
   }, {
     key: 'Fields',
@@ -173,16 +197,225 @@ var Abstract = function () {
   return Abstract;
 }();
 
-var Products = function (_Abstract) {
-  _inherits(Products, _Abstract);
+var Brands = function (_Abstract) {
+  _inherits(Brands, _Abstract);
+
+  function Brands(endpoint) {
+    _classCallCheck(this, Brands);
+
+    var _this = _possibleConstructorReturn(this, (Brands.__proto__ || Object.getPrototypeOf(Brands)).call(this, endpoint));
+
+    _this.endpoint = 'brands';
+    return _this;
+  }
+
+  return Brands;
+}(Abstract);
+
+var Cart = function (_Abstract2) {
+  _inherits(Cart, _Abstract2);
+
+  function Cart(endpoint) {
+    _classCallCheck(this, Cart);
+
+    var _this2 = _possibleConstructorReturn(this, (Cart.__proto__ || Object.getPrototypeOf(Cart)).call(this, endpoint));
+
+    _this2.endpoint = 'carts';
+    _this2.cartId = _this2.Identifier();
+    return _this2;
+  }
+
+  _createClass(Cart, [{
+    key: 'Identifier',
+    value: function Identifier() {
+      var reset = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
+      var id = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+
+
+      if (!reset && !id && this.m.Storage.get('mcart') !== null) {
+        return this.m.Storage.get('mcart');
+      }
+
+      if (!id) {
+        id = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'.replace(/[x]/g, function () {
+          return (Math.random() * 16 | 0).toString(16);
+        });
+      }
+
+      this.m.Storage.set('mcart', id);
+
+      this.cartId = id;
+
+      return id;
+    }
+  }, {
+    key: 'Contents',
+    value: function Contents(callback, error) {
+      return this.m.Request(this.endpoint + '/' + this.cartId + '/items', 'GET', callback, error);
+    }
+  }, {
+    key: 'Insert',
+    value: function Insert(product, quantity, callback, error) {
+      var productObject = {
+        id: product,
+        quantity: parseInt(quantity) || 1
+      };
+
+      return this.m.Request(this.endpoint + '/' + this.cartId + '/items', 'POST', productObject, callback, error);
+    }
+  }, {
+    key: 'Remove',
+    value: function Remove(id, callback, error) {
+      return this.m.Request(this.endpoint + '/' + this.cartId + '/items/' + id, 'DELETE', callback, error);
+    }
+  }, {
+    key: 'Quantity',
+    value: function Quantity(id, quantity, callback, error) {
+      var quantityInt = parseInt(quantity);
+
+      return this.m.Request(this.endpoint + '/' + this.cartId + '/items/' + id, 'PUT', [{
+        id: id,
+        quantity: quantityInt
+      }], callback, error);
+    }
+  }, {
+    key: 'Complete',
+    value: function Complete(data, callback, error) {
+      return this.m.Request(this.endpoint + '/' + this.cartId + '/checkout', 'POST', data, callback, error);
+    }
+  }, {
+    key: 'Delete',
+    value: function Delete(callback, error) {
+      return this.m.Request(this.endpoint + '/' + this.cartId, 'DELETE', callback, error);
+    }
+  }]);
+
+  return Cart;
+}(Abstract);
+
+var Categories = function (_Abstract3) {
+  _inherits(Categories, _Abstract3);
+
+  function Categories(endpoint) {
+    _classCallCheck(this, Categories);
+
+    var _this3 = _possibleConstructorReturn(this, (Categories.__proto__ || Object.getPrototypeOf(Categories)).call(this, endpoint));
+
+    _this3.endpoint = 'categories';
+    return _this3;
+  }
+
+  _createClass(Categories, [{
+    key: 'Tree',
+    value: function Tree(error, callback) {
+      return this.m.Request(this.endpoint + '/tree', 'GET', error, callback);
+    }
+  }]);
+
+  return Categories;
+}(Abstract);
+
+var Collections = function (_Abstract4) {
+  _inherits(Collections, _Abstract4);
+
+  function Collections(endpoint) {
+    _classCallCheck(this, Collections);
+
+    var _this4 = _possibleConstructorReturn(this, (Collections.__proto__ || Object.getPrototypeOf(Collections)).call(this, endpoint));
+
+    _this4.endpoint = 'collections';
+    return _this4;
+  }
+
+  return Collections;
+}(Abstract);
+
+var Currency = function (_Abstract5) {
+  _inherits(Currency, _Abstract5);
+
+  function Currency(endpoint) {
+    _classCallCheck(this, Currency);
+
+    var _this5 = _possibleConstructorReturn(this, (Currency.__proto__ || Object.getPrototypeOf(Currency)).call(this, endpoint));
+
+    _this5.endpoint = 'currencies';
+    return _this5;
+  }
+
+  _createClass(Currency, [{
+    key: 'Set',
+    value: function Set(currency) {
+      var _this6 = this;
+
+      this.m.Storage.set('mcurrency', currency);
+      this.m.config.currency = currency;
+
+      var promise = new Promise(function (resolve, reject) {
+        var request = _this6.m.Storage.get('mcurrency');
+
+        try {
+          resolve(request);
+        } catch (err) {
+          reject(new Error(err));
+        }
+      });
+
+      return promise;
+    }
+  }, {
+    key: 'Active',
+    value: function Active() {
+      var _this7 = this;
+
+      var promise = new Promise(function (resolve, reject) {
+        var request = _this7.m.Storage.get('mcurrency');
+
+        try {
+          resolve(request);
+        } catch (err) {
+          reject(new Error(err));
+        }
+      });
+
+      return promise;
+    }
+  }]);
+
+  return Currency;
+}(Abstract);
+
+var Orders = function (_Abstract6) {
+  _inherits(Orders, _Abstract6);
+
+  function Orders(endpoint) {
+    _classCallCheck(this, Orders);
+
+    var _this8 = _possibleConstructorReturn(this, (Orders.__proto__ || Object.getPrototypeOf(Orders)).call(this, endpoint));
+
+    _this8.endpoint = 'orders';
+    return _this8;
+  }
+
+  _createClass(Orders, [{
+    key: 'Payment',
+    value: function Payment(id, data, callback, error) {
+      return this.m.Request(this.endpoint + '/' + id + '/payments', 'POST', data, callback, error);
+    }
+  }]);
+
+  return Orders;
+}(Abstract);
+
+var Products = function (_Abstract7) {
+  _inherits(Products, _Abstract7);
 
   function Products(endpoint) {
     _classCallCheck(this, Products);
 
-    var _this = _possibleConstructorReturn(this, (Products.__proto__ || Object.getPrototypeOf(Products)).call(this, endpoint));
+    var _this9 = _possibleConstructorReturn(this, (Products.__proto__ || Object.getPrototypeOf(Products)).call(this, endpoint));
 
-    _this.endpoint = 'products';
-    return _this;
+    _this9.endpoint = 'products';
+    return _this9;
   }
 
   _createClass(Products, [{
@@ -296,23 +529,21 @@ var RequestFactory = function () {
   _createClass(RequestFactory, [{
     key: 'make',
     value: function make(uri, method, data, headers) {
-      var _this2 = this;
+      var _this10 = this;
 
+      console.log(data);
       method = method.toUpperCase();
       var url = this.m.config.protocol + '://' + this.m.config.host + (uri !== 'oauth/access_token' ? '/' + this.m.config.version + '/' + uri : '/' + uri);
 
-      if (method === 'GET') {
-        url += '?' + this.m.Helper.Serialize(data);
-        data = null;
-      } else {
-        data = this.m.Helper.Serialize(data);
+      if (method === 'POST' || method === 'PUT') {
+        data = uri === 'oauth/access_token' ? this.m.Helper.Serialize(data) : '{"data":' + JSON.stringify(data) + '}';
       }
 
       this.driver.open(method, url, true);
 
       var timeout = setTimeout(function () {
-        _this2.driver.abort();
-        return _this2.driver.error(_this2.driver, 408, 'Your request timed out');
+        _this10.driver.abort();
+        return _this10.driver.error(_this10.driver, 408, 'Your request timed out');
       }, this.m.config.timeout);
 
       for (var k in headers) {
