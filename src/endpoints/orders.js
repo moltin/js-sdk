a2903eeaa9ceac968423f50f77d4d72fa0669efa
Matthew Foyle <mfoyle@Matthews-MacBook-Pro.local>
2017-03-06 11:39:15 +0000

    added Order Items call
    to src/endpoints/orders.js
    
    You can now access order items

--- src/endpoints/orders.js
+++ src/endpoints/orders.js
@@ -11,6 +11,10 @@ class OrdersEndpoint extends BaseExtend {
     return this.request.send(`${this.endpoint}`, 'GET');
   }
 
+  Items(id) {
+    return this.request.send(`${this.endpoint}/${id}/items`, 'GET');
+  }
+
   Payment(id, body) {
     return this.request.send(`${this.endpoint}/${id}/payments`, 'POST', body);
   }
