b6cdd2e3bc3b5b3e6334930cd390251f618766bc
Jonathan Steele <ynnoj@users.noreply.github.com>
2017-08-30 12:55:37 +0100

    refactor: Remove redundant configuration keys

--- src/config.js
+++ src/config.js
@@ -6,18 +6,13 @@ class Config {
     this.client_id = options.client_id;
     this.client_secret = options.client_secret;
     this.host = 'api.moltin.com';
-    this.port = '443';
     this.protocol = 'https';
     this.version = 'v2';
-    this.debug = false;
     this.currency = options.currency;
-    this.language = false;
-    this.timeout = 60000;
     this.auth = {
       expires: 3600,
       uri: 'oauth/access_token',
     };
-    this.methods = ['GET', 'POST', 'PUT', 'DELETE'];
     this.sdk = {
       version,
       language: 'JS',
