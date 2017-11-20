8dcce9960d38b8b01cbf17d0bb7e91490b24a57f
Jonathan Steele <ynnoj@users.noreply.github.com>
2017-11-20 16:01:47 +0000

    feat: AddPromotion method for cart class

--- src/endpoints/cart.js
+++ src/endpoints/cart.js
@@ -32,6 +32,15 @@ class CartEndpoint extends BaseExtend {
     return this.request.send(`${this.endpoint}/${cartId}/items`, 'POST', itemObject);
   }
 
+  AddPromotion(code, cartId = this.cartId) {
+    const body = {
+      type: 'promotion_item',
+      code,
+    };
+
+    return this.request.send(`${this.endpoint}/${cartId}/items`, 'POST', body);
+  }
+
   RemoveItem(itemId, cartId = this.cartId) {
     return this.request.send(`${this.endpoint}/${cartId}/items/${itemId}`, 'DELETE');
   }
