b209f9d0a051f65193384663b81aeb107b45a695
Jamie Barton <hi@jamiebarton.co.uk>
2018-03-08 13:47:37 +0000

    feat: Add Allocate/Deallocate functionality

--- src/endpoints/inventories.js
+++ src/endpoints/inventories.js
@@ -2,38 +2,77 @@ import BaseExtend from '../extends/base'
 
 class InventoriesEndpoint extends BaseExtend {
   constructor(endpoint) {
-    super(endpoint)
+    super(endpoint);
 
-    this.endpoint = 'inventories'
+    this.endpoint = 'inventories';
+
+    this.transactionObject = {
+      type: 'stock-transaction'
+    };
   }
 
   Get(productId) {
-    return this.request.send(`${this.endpoint}/${productId}`, 'GET')
+    return this.request.send(`${this.endpoint}/${productId}`, 'GET');
   }
 
   IncrementStock(productId, quantity) {
-    const body = {
-      type: 'stock-transaction',
+    const body = Object.assign({}, this.transactionObject, {
       action: 'increment',
       quantity
-    }
+    });
 
-    return this.request.send(`${this.endpoint}/${productId}/transactions`, 'POST', body)
+    return this.request.send(
+      `${this.endpoint}/${productId}/transactions`,
+      'POST',
+      body
+    );
   }
 
   DecrementStock(productId, quantity) {
-    const body = {
-      type: 'stock-transaction',
+    const body = Object.assign({}, this.transactionObject, {
       action: 'decrement',
       quantity
-    }
+    });
 
-    return this.request.send(`${this.endpoint}/${productId}/transactions`, 'POST', body)
+    return this.request.send(
+      `${this.endpoint}/${productId}/transactions`,
+      'POST',
+      body
+    );
+  }
+
+  AllocateStock(productId, quantity) {
+    const body = Object.assign({}, this.transactionObject, {
+      action: 'allocate',
+      quantity
+    });
+
+    return this.request.send(
+      `${this.endpoint}/${productId}/transactions`,
+      'POST',
+      body
+    );
+  }
+
+  DeallocateStock(productId, quantity) {
+    const body = Object.assign({}, this.transactionObject, {
+      action: 'deallocate',
+      quantity
+    });
+
+    return this.request.send(
+      `${this.endpoint}/${productId}/transactions`,
+      'POST',
+      body
+    );
   }
 
   GetTransactions(productId) {
-    return this.request.send(`${this.endpoint}/${productId}/transactions`, 'GET')
+    return this.request.send(
+      `${this.endpoint}/${productId}/transactions`,
+      'GET'
+    );
   }
 }
 
-export default InventoriesEndpoint
+export default InventoriesEndpoint;
