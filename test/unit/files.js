398952281fba1fcc3afcb0b4dd91278d52703ee2
Jonathan Steele <ynnoj@users.noreply.github.com>
2018-03-22 12:12:48 +0000

    test: Add failing tests for product-main_image relationship requests

--- test/unit/files.js
+++ test/unit/files.js
@@ -140,4 +140,67 @@ describe('Moltin files', () => {
       assert.deepEqual(response, { data: [] });
     });
   });
+
+  it('should create a product-main_image relationship', () => {
+    // Intercept the API request
+    nock(apiUrl, {
+      reqheaders: {
+        Authorization: 'Bearer: a550d8cbd4a4627013452359ab69694cd446615a',
+      },
+    })
+    .post('/products/product-1/relationships/main-image', {
+      data: {
+        type: 'main_image',
+        id: 'file-1',
+      },
+    })
+    .reply(200, files[0]);
+
+    return Moltin.Products.CreateRelationships(products[0].id, 'main-image', files[0].id)
+    .then((response) => {
+      assert.propertyVal(response, 'id', 'file-1');
+    });
+  });
+
+  it('should delete a product-main_image relationship', () => {
+    // Intercept the API request
+    nock(apiUrl, {
+      reqheaders: {
+        Authorization: 'Bearer: a550d8cbd4a4627013452359ab69694cd446615a',
+      },
+    })
+    .delete('/products/product-1/relationships/main-image', {
+      data: {
+        type: 'main_image',
+        id: 'file-1',
+      },
+    })
+    .reply(204);
+
+    return Moltin.Products.DeleteRelationships(products[0].id, 'main-image', files[0].id)
+    .then((response) => {
+      assert.equal(response, '{}');
+    });
+  });
+
+  it('should update the product-main_image relationship', () => {
+    // Intercept the API request
+    nock(apiUrl, {
+      reqheaders: {
+        Authorization: 'Bearer: a550d8cbd4a4627013452359ab69694cd446615a',
+      },
+    })
+    .put('/products/product-1/relationships/main-image', {
+      data: {
+        type: 'main_image',
+        id: 'file-1',
+      },
+    })
+    .reply(204);
+
+    return Moltin.Products.UpdateRelationships(products[0].id, 'main-image', files[0].id)
+    .then((response) => {
+      assert.equal(response, '{}');
+    });
+  });
 });
