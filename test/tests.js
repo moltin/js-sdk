fc010209e3602dd651b14e0d6480b2960f1be307
Jonathan Steele <ynnoj@users.noreply.github.com>
2018-03-28 17:27:43 +0100

    test: Run authentication tests first

---
+++ test/tests.js
@@ -0,0 +1,19 @@
+require('./unit/authentication');
+
+// Classes
+require('./unit/addresses');
+require('./unit/brands');
+require('./unit/cart');
+require('./unit/categories');
+require('./unit/collections');
+require('./unit/customers');
+require('./unit/fields');
+require('./unit/files');
+require('./unit/flows');
+require('./unit/gateways');
+require('./unit/inventories');
+require('./unit/orders');
+require('./unit/products');
+
+// Utilities
+require('./utils/helpers');
