c6ae984d2c13c4a3e31ddd91f5298fe2c77b061c
Jamie Barton <hi@jamiebarton.co.uk>
2018-05-02 13:38:25 +0100

    feat: Add all Transactions/Capture methods

---
+++ src/endpoints/transactions.js
@@ -0,0 +1,24 @@
+import { singularize } from 'inflected'
+
+import BaseExtend from '../extends/base'
+
+class TransactionsEndpoint extends BaseExtend {
+  constructor(endpoint) {
+    super(endpoint)
+
+    this.endpoint = 'transactions'
+  }
+
+  All({ order }) {
+    return this.request.send(`orders/${order}/${this.endpoint}`, 'GET')
+  }
+
+  Capture({ order, transaction }) {
+    return this.request.send(
+      `orders/${order}/transactions/${transaction}/capture`,
+      'POST'
+    )
+  }
+}
+
+export default TransactionsEndpoint
