8a99f41db5107a4c1448de33bd543ed0e204e62c
Craig Tweedy <ahoy@craigtweedy.co.uk>
2019-01-15 14:21:10 +0000

    feat: Added support for tax endpoints, extends cart items endpoint to allow includes (#240)

--- src/moltin.d.ts
+++ src/moltin.d.ts
@@ -327,6 +327,13 @@ export namespace moltin {
     instructions?: string
   }
 
+  export interface ItemTaxObject {
+    name: string
+    jurisdiction: string
+    code: string
+    rate: number
+  }
+
   // NOTE: The implementation of `CartEndpoint` extends `BaseExtend` however it breaks
   // polymorphism my defining an incompatible signature for `constructor` so having to
   // redeclare the BaseExtend methods.
@@ -361,6 +368,8 @@ export namespace moltin {
      * @deprecated Use UpdateItem method
      */
     UpdateItemQuantity<T = any>(itemId: string, quantity: number): Promise<T>
+    AddItemTax<T = any>(itemId: string, taxData: ItemTaxObject)
+    RemoveItemTax<T = any>(itemId: string, taxItemId: string)
     Checkout<T = any>(
       customer: CheckoutCustomer | CheckoutCustomerObject,
       billingAddress: Address,
