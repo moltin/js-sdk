6d67b63330211f384610e38818baf4f6cd1dccc9
Dusan Radovanovic <dusan.radovanovic@elasticpath.com>
2020-07-10 01:06:06 -0700

    Removed nested namespaces

--- src/types/field.d.ts
+++ src/types/field.d.ts
@@ -5,34 +5,31 @@
  * validation rules.
  * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/advanced/custom-data/fields.html
  */
-import { core, Relationship } from './core'
+import { Relationship, CrudQueryableResource } from './core';
 
-export as namespace field
 
-export namespace field {
-  /**
-   * Core Field Base Interface
-   * For custom flows, extend this interface
-   * DOCS:
-   */
-  export interface FieldBase {
-    id?: string
-    type: string
-    name: string
-    slug: string
-    field_type: string
-    validation_rules: any[]
-    description: string
-    required: boolean
-    default: any
-    enabled: boolean
-    order: number
-    relationships: Relationship<'addresses' | 'products' | 'brands' | 'collections' | 'categories' | 'customers' |
-      'cart_items' | 'orders' | 'order_items' | 'promotions' | 'flows'>
-    omit_null: boolean
-  }
+/**
+ * Core Field Base Interface
+ * For custom flows, extend this interface
+ * DOCS:
+ */
+export interface FieldBase {
+  id?: string
+  type: string
+  name: string
+  slug: string
+  field_type: string
+  validation_rules: any[]
+  description: string
+  required: boolean
+  default: any
+  enabled: boolean
+  order: number
+  relationships: Relationship<'addresses' | 'products' | 'brands' | 'collections' | 'categories' | 'customers' |
+    'cart_items' | 'orders' | 'order_items' | 'promotions' | 'flows'>
+  omit_null: boolean
+}
 
-  export interface FieldsEndpoint extends core.CrudQueryableResource<FieldBase, null, null, null> {
-    endpoint: 'fields'
-  }
+export interface FieldsEndpoint extends CrudQueryableResource<FieldBase, null, null, null> {
+  endpoint: 'fields'
 }
--- src/types/flow.d.ts
+++ src/types/flow.d.ts
@@ -4,47 +4,44 @@
  * associate it with. For example a Flow with a slug of products will be applied to all Product responses in your store.
  * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/advanced/custom-data/flows.html
  */
-import { core } from './core'
+import { CrudQueryableResource } from './core';
 
-export as namespace flow
 
-export namespace flow {
-  /**
-   * Core Flow Base Interface
-   */
-  export interface FlowBase {
-    id?: string
-    slug: 'addresses' | 'products' | 'brands' | 'collections' | 'categories' | 'customers' | 'cart_items' | 'orders' | 'order_items' | 'promotions'
-    type: string
-    name: string
-    description: string
-    enabled: boolean
-  }
+/**
+ * Core Flow Base Interface
+ */
+export interface FlowBase {
+  id?: string
+  slug: 'addresses' | 'products' | 'brands' | 'collections' | 'categories' | 'customers' | 'cart_items' | 'orders' | 'order_items' | 'promotions'
+  type: string
+  name: string
+  description: string
+  enabled: boolean
+}
 
-  export interface FlowFilter {
-  }
+export interface FlowFilter {
+}
 
-  /**
-   * Flow Endpoints
-   */
-  export interface FlowEndpoint extends core.CrudQueryableResource<FlowBase, FlowFilter, null, null> {
-    endpoint: 'flow'
+/**
+ * Flow Endpoints
+ */
+export interface FlowEndpoint extends CrudQueryableResource<FlowBase, FlowFilter, null, null> {
+  endpoint: 'flow'
 
-    GetEntries<T = any>(slug: string): Promise<T>
+  GetEntries<T = any>(slug: string): Promise<T>
 
-    GetEntry<T = any>(slug: string, entryId: string): Promise<T>
+  GetEntry<T = any>(slug: string, entryId: string): Promise<T>
 
-    CreateEntry<RequestBody = any, ResponseBody = any>(
-      slug: string,
-      body: RequestBody
-    ): Promise<ResponseBody>
+  CreateEntry<RequestBody = any, ResponseBody = any>(
+    slug: string,
+    body: RequestBody
+  ): Promise<ResponseBody>
 
-    UpdateEntry<RequestBody = any, ResponseBody = any>(
-      slug: string,
-      entryId: string,
-      body: RequestBody
-    ): Promise<ResponseBody>
+  UpdateEntry<RequestBody = any, ResponseBody = any>(
+    slug: string,
+    entryId: string,
+    body: RequestBody
+  ): Promise<ResponseBody>
 
-    DeleteEntry<T = any>(slug: string, entryId: string): Promise<T>
-  }
+  DeleteEntry<T = any>(slug: string, entryId: string): Promise<T>
 }
