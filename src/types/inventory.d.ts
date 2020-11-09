a400b3122f8b59948d476a5890589284ac893a29
Dubno Petro <34708483+petro97@users.noreply.github.com>
2020-11-09 20:26:23 +0200

    feat: Add docs for inventory endpoints. Remove duplicate Get function for inventories (#359)

--- src/types/inventory.d.ts
+++ src/types/inventory.d.ts
@@ -4,17 +4,31 @@
  * history of inventory transactions, enabling easier stock auditing.
  * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/catalog/inventory/index.html
  */
-import { Identifiable, QueryableResource, ResourceList } from './core';
+import { Identifiable, QueryableResource, ResourceList } from './core'
+
+/**
+ * Update Inventory Interface
+ * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/catalog/inventory/update-inventory.html
+ */
+export interface InventoryResponse {
+  type: string
+  action: string
+  product_id: string
+  quantity: number
+  timestamps: {
+    created_at: string
+  }
+}
 
 /**
  * Core Inventory Base Interface
- * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/catalog/products/index.html
+ * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/catalog/inventory/index.html
  */
 export interface InventoryBase {
-  type: string;
-  total: number;
-  available: number;
-  allocated: number;
+  type: string
+  total: number
+  available: number
+  allocated: number
 }
 
 export interface Inventory extends Identifiable, InventoryBase {
@@ -30,11 +44,56 @@ export interface InventoryEndpoint extends QueryableResource<
   never,
   never
 > {
-  endpoint: 'inventory';
+  endpoint: 'inventory'
 
-  IncrementStock(productId: string, quantity: number): Promise<InventoryBase>;
-  DecrementStock(productId: string, quantity: number): Promise<InventoryBase>;
-  AllocateStock(productId: string, quantity: number): Promise<InventoryBase>;
-  DeallocateStock(productId: string, quantity: number): Promise<InventoryBase>;
-  GetTransactions(productId: string): Promise<ResourceList<InventoryBase>>;
+  /**
+   * Increment Stock
+   * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/catalog/inventory/update-inventory.html
+   * @param productId The ID for the product you’re performing this action on.
+   * @param quantity The amount of stock affected by this transaction.
+   */
+  IncrementStock(
+    productId: string,
+    quantity: number
+  ): Promise<InventoryResponse>
+
+  /**
+   * Decrement Stock
+   * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/catalog/inventory/update-inventory.html
+   * @param productId The ID for the product you’re performing this action on.
+   * @param quantity The amount of stock affected by this transaction.
+   */
+  DecrementStock(
+    productId: string,
+    quantity: number
+  ): Promise<InventoryResponse>
+
+  /**
+   * Allocate Stock
+   * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/catalog/inventory/update-inventory.html
+   * @param productId The ID for the product you’re performing this action on.
+   * @param quantity The amount of stock affected by this transaction.
+   */
+  AllocateStock(
+    productId: string,
+    quantity: number
+  ): Promise<InventoryResponse>
+
+  /**
+   * Deallocate Stock
+   * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/catalog/inventory/update-inventory.html
+   * @param productId The ID for the product you’re performing this action on.
+   * @param quantity The amount of stock affected by this transaction.
+   */
+  DeallocateStock(
+    productId: string,
+    quantity: number
+  ): Promise<InventoryResponse>
+
+  /**
+   * Get Transactions
+   * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/catalog/inventory/stock-transactions.html
+   * @param productId The ID for the product you’re performing this action on.
+   */
+  GetTransactions(productId: string): Promise<ResourceList<InventoryBase>>
 }
