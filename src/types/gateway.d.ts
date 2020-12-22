0c5b2e7811177200ef19ef142fedb5ea85034b8d
Yurii Khovzun <yurii.khovzun@bitcom.systems>
2020-12-22 10:57:05 +0200

    fix: types

--- src/types/gateway.d.ts
+++ src/types/gateway.d.ts
@@ -4,7 +4,7 @@
  * You can enable and disable built-in payment gateways.
  * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/payments/gateways/index.html
  */
-import { Attributes, QueryableResource} from './core'
+import { Attributes, QueryableResource, Resource } from './core'
 
 /**
  * Core Gateway Base Interface
@@ -14,7 +14,7 @@ export  interface  GatewayBase {
   name: string
   slug: string
   type: string
-  enable: boolean
+  enabled: boolean
   login?: string
   merchant_id?:string
   password?:string
@@ -36,7 +36,7 @@ export  interface  Gateway extends GatewayBase {}
 export interface GatewaysEndpoint extends QueryableResource<Gateway, never, never, never> {
   endpoint:'gateways'
 
-  Update<R = any, T = Gateway>(
+  Update<R = any, T = Resource<Gateway>>(
     slug: string,
     body: R
   ): Promise<T>
@@ -48,7 +48,7 @@ export interface GatewaysEndpoint extends QueryableResource<Gateway, never, neve
    * @param slug [string] The slug of the gateway to be updated.
    * @param enabled [boolean] true or false depending on the enabled status.
    */
-  Enabled<T>(slug: string, enabled: boolean): Promise<T>
+  Enabled<T = Resource<Gateway>>(slug: string, enabled: boolean): Promise<T>
 
   GetSlugAttributes(flowType: string, token?: string): Promise<Attributes>
 
