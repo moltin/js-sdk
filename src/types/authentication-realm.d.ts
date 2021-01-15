768b8555fcfbc7eaf448fc1fcca6e5242b43fc3c
Rostyk <34774987+rostyk-kanafotskyy@users.noreply.github.com>
2021-01-15 17:44:28 +0200

    fix: auth settings type, added docs
    - Added settings type
     - Added missing docs for auth settings and auth realms
     - Added missing docs for product

--- src/types/authentication-realm.d.ts
+++ src/types/authentication-realm.d.ts
@@ -1,30 +1,52 @@
+/**
+ * Authentication Realms
+ * Description: You can use the Authentication Realms endpoint to configure authentication options.
+ * Authentication Realm sub-resources can be used to configure single sign-on.
+ * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/index.html
+ */
 import { CrudQueryableResource } from './core'
+
+/**
+ * The Authentication Realm object Interface
+ * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/index.html#the-authentication-realm-object
+ */
 
 export interface Realm extends RealmBase {
   meta: {
     timestamps: {
-      created_at: string;
-      updated_at: string;
-    };
-  };
-  links:{}
+      created_at: string
+      updated_at: string
+    }
+  }
+  links: {}
 }
 
 export interface RealmBase {
-  id: string;
-  name: string;
+  id: string
+  name: string
 }
 
 export interface RealmCreateBody {
-  type: string;
-  name: string;
+  type: string
+  name: string
 }
 
 /**
- * TODO: TODO: Need to point to documentation once ready
-* AuthenticationRealm Endpoints
-*/
-export interface AuthenticationRealmEndpoint extends CrudQueryableResource<Realm, RealmCreateBody, RealmCreateBody, never, never, never> {
+ * Authentication Realms Endpoints
+ * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/get-all-authentication-realms.html
+ * Get DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/get-an-authentication-realm.html
+ * Get All DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/get-all-authentication-realms.html#get-get-a-list-of-authentication-realms
+ * Update DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/update-an-authentication-realm.html
+ */
+export interface AuthenticationRealmEndpoint
+  extends CrudQueryableResource<
+      Realm,
+      RealmCreateBody,
+      RealmCreateBody,
+      never,
+      never,
+      never
+    > {
   endpoint: 'authentication-realm'
   storage: Storage
 }
