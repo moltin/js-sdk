8df21722564444c5afe992a5accc0fd56167b260
Rostyk <34774987+rostyk-kanafotskyy@users.noreply.github.com>
2021-01-20 19:56:20 +0200

    fix: added redirect_uris param, updated realms CRUD docs (#382)

--- src/types/authentication-realm.d.ts
+++ src/types/authentication-realm.d.ts
@@ -4,7 +4,7 @@
  * Authentication Realm sub-resources can be used to configure single sign-on.
  * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/index.html
  */
-import { CrudQueryableResource } from './core'
+import { CrudQueryableResource, Resource, ResourceList } from './core'
 
 /**
  * The Authentication Realm object Interface
@@ -24,29 +24,64 @@ export interface Realm extends RealmBase {
 export interface RealmBase {
   id: string
   name: string
+  redirect_uris: string[]
 }
 
 export interface RealmCreateBody {
   type: string
   name: string
+  redirect_uris: string[]
 }
 
 /**
  * Authentication Realms Endpoints
  * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/get-all-authentication-realms.html
- * Get DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/get-an-authentication-realm.html
- * Get All DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/get-all-authentication-realms.html#get-get-a-list-of-authentication-realms
- * Update DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/update-an-authentication-realm.html
  */
 export interface AuthenticationRealmEndpoint
-  extends CrudQueryableResource<
+  extends Omit<
+    CrudQueryableResource<
       Realm,
       RealmCreateBody,
       RealmCreateBody,
       never,
       never,
       never
-    > {
+    >,
+    'All' | 'Create' | 'Update' | 'Get'
+  > {
   endpoint: 'authentication-realm'
   storage: Storage
+  /**
+   * Get all Authentication Realms
+   * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/get-all-authentication-realms.html
+   * @param token - The Bearer token to grant access to the API.
+   * @param headers
+   */
+  All(token?: string, headers?): Promise<ResourceList<Realm>>
+
+  /**
+   * Get a Realm by reference
+   * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/get-an-authentication-realm.html
+   * @param data.realmId - The ID for the requested authentication-realm,
+   * @param data.token - The Bearer token to grant access to the API.
+   */
+  Get(data: { realmId: string; token?: string }): Promise<Resource<Realm>>
+
+  /**
+   * Create an Authentication Realm
+   */
+  Create(body: Partial<RealmBase>): Promise<Resource<Realm>>
+
+  /**
+   * Update an Authentication Realm
+   * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/authentication-realms/get-an-authentication-realm.html
+   * @param realmId - The ID for the requested authentication-realm.
+   * @param body - The Authentication Realm object
+   * @param token - The Bearer token to grant access to the API.
+   */
+  Update(
+    realmId: string,
+    body: Partial<RealmBase>,
+    token?: string
+  ): Promise<Resource<Realm>>
 }
