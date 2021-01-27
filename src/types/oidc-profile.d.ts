e900c2c4405ed528644a431b9da98002890daaee
Rostyk <34774987+rostyk-kanafotskyy@users.noreply.github.com>
2021-01-27 19:47:23 +0200

    fix: updated OpenID profile endpoint types (#390)
    * fix: updated OpenID profile endpoint types
    
    * fix: typo

--- src/types/oidc-profile.d.ts
+++ src/types/oidc-profile.d.ts
@@ -1,60 +1,99 @@
-import { Resource, ResourcePage } from './core'
+/**
+ * OpenID Connect Profiles
+ * Description: An OpenID Connect Profile resource represents a specific configuration of an OpenID Connect Provider.
+ * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/oidc-profiles/index.html
+ */
+import { Identifiable, Resource, ResourcePage } from './core'
+
+/**
+ * The OpenID Connect Profile object
+ * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/oidc-profiles/index.html#the-openid-connect-profile-object
+ */
 
 export interface ProfileBase {
-  id: string;
-  name: string;
-  type: string;
-  discovery_url: string;
-  client_id: string;
+  name: string
+  discovery_url: string
+  client_id: string
+  client_secret: string
+}
+
+export interface ProfileBody extends Partial<ProfileBase> {
+  type: string
+}
+export interface Profile extends ProfileBase, Identifiable {
+  type: string
   meta: {
     timestamps: {
-      created_at: string;
-      updated_at: string;
-    };
-  };
+      created_at: string
+      updated_at: string
+    }
+  }
 }
 
-export interface ProfileBody {
-  name: string;
-  type: string;
-  discovery_url: string;
-  client_id: string;
-  client_secret: string;
-}
-
-export interface Profile extends Resource<ProfileBase> {
+export interface ProfileListItem extends Profile {
   links: {
-    "authorization-endpoint": string,
-    "client-discovery-url": string,
-    "self": string,
+    'authorization-endpoint': string
+    'client-discovery-url': string
+    self: string
+    'callback-endpoint': string
+  }
+}
+
+export interface ProfileResponse extends Resource<ProfileBase> {
+  links: {
+    'authorization-endpoint': string
+    'client-discovery-url': string
+    self: string
+    'callback-endpoint': string
   }
 }
 
 /**
- * TODO: Need to point to documentation once ready
- * AuthenticationRealm Endpoints
+ * OpenID Profiles Endpoints
+ * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/oidc-profiles/get-all-oidc-profiles.html
  */
 export interface OidcProfileEndpoint {
-  Get({
-    realmId,
-    profileId
-  }): Promise<Profile>
+  /**
+   * Get all OpenID Connect Profiles
+   * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/oidc-profiles/get-all-oidc-profiles.html
+   * @param realmId - The ID for the authentication-realm containing the OpenID Connect profiles.
+   */
+  All(realmId: string): Promise<ResourcePage<ProfileListItem>>
 
-  All(realmId: string): Promise<ResourcePage<Profile>>
+  /**
+   * Get an OpenID Connect Profile
+   * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/oidc-profiles/get-an-oidc-profile.html
+   * @param realmId - The ID for the authentication-realm containing the OpenID Connect profiles.
+   * @param profileId - The ID for the requested OpenID Connect profile.
+   */
+  Get({ realmId, profileId }): Promise<ProfileResponse>
 
-  Create(
-    realmId: string,
-    body: ProfileBody
-  ): Promise<Resource<ProfileBody>>
+  /**
+   * Create an OpenID Connect Profile
+   * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/oidc-profiles/create-an-oidc-profile.html
+   * @param realmId - The ID for the authentication-realm containing the OpenID Connect profiles.
+   * @param body - The OpenID Connect Profile object
+   */
+  Create(realmId: string, body: { data: ProfileBody }): Promise<ProfileResponse>
 
+  /**
+   * Update an OpenID Connect Profile
+   * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/oidc-profiles/update-an-oidc-profile.html
+   * @param realmId - The ID for the authentication-realm containing the OpenID Connect profiles.
+   * @param profileId - The ID for the requested OpenID Connect profile.
+   * @param body - The OpenID Connect Profile object
+   */
   Update(
     realmId: string,
     profileId: string,
-    body: ProfileBody
-  ): Promise<Resource<ProfileBody>>
+    body: { data: ProfileBody }
+  ): Promise<ProfileResponse>
 
-  Delete(
-    realmId: string,
-    profileId: string
-  )
+  /**
+   * Delete an OpenID Connect Profile
+   * DOCS: https://documentation.elasticpath.com/commerce-cloud/docs/api/single-sign-on/oidc-profiles/delete-an-oidc-profile.html
+   * @param realmId - The ID for the authentication-realm containing the OpenID Connect profiles.
+   * @param profileId - The ID for the requested OpenID Connect profile.
+   */
+  Delete(realmId: string, profileId: string)
 }
