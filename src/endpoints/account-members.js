1115d2ca9344e767e0dfb0a030781089e8a51935
Alex Razkevich <razkevich8@gmail.com>
2021-02-25 13:30:14 +0300

    [MT-6386] Added Account Members

---
+++ src/endpoints/account-members.js
@@ -0,0 +1,19 @@
+import CRUDExtend from '../extends/crud'
+
+class AccountMembersEndpoint extends CRUDExtend {
+    constructor(endpoint) {
+        super(endpoint)
+        this.endpoint = 'account-members'
+    }
+
+    Get({accountMemberId, token = null}) {
+        return this.request.send(
+            `${this.endpoint}/${accountMemberId}`,
+            'GET',
+            undefined,
+            token
+        )
+    }
+}
+
+export default AccountMembersEndpoint
