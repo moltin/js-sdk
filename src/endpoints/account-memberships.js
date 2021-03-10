949257696d93a740fbcdcb5e9422ed9d4adc5897
Alex Razkevich <razkevich8@gmail.com>
2021-03-10 11:35:26 +0300

    [MT-6474] Added Account Memberships

--- src/endpoints/account-memberships.js
+++ src/endpoints/account-memberships.js
@@ -1,31 +1,35 @@
 import CRUDExtend from '../extends/crud'
 
 class AccountMembershipsEndpoint extends CRUDExtend {
-    constructor(endpoint) {
-        super(endpoint)
-    }
+  constructor(endpoint) {
+    super(endpoint)
+  }
 
-    Create(accountId, body, token = null) {
-        return this.request.send(`accounts/${accountId}/account-memberships`, 'POST', body.data)
-    }
+  Create(accountId, body, token = null) {
+    return this.request.send(
+      `accounts/${accountId}/account-memberships`,
+      'POST',
+      body.data
+    )
+  }
 
-    Get(accountId, accountMembershipId, token = null) {
-        return this.request.send(
-            `accounts/${accountId}/account-memberships/${accountMembershipId}`,
-            'GET',
-            undefined,
-            token
-        )
-    }
+  Get(accountId, accountMembershipId, token = null) {
+    return this.request.send(
+      `accounts/${accountId}/account-memberships/${accountMembershipId}`,
+      'GET',
+      undefined,
+      token
+    )
+  }
 
-    Delete(accountId, accountMembershipId, token = null) {
-        return this.request.send(
-            `accounts/${accountId}/account-memberships/${accountMembershipId}`,
-            'DELETE',
-            undefined,
-            token
-        )
-    }
+  Delete(accountId, accountMembershipId, token = null) {
+    return this.request.send(
+      `accounts/${accountId}/account-memberships/${accountMembershipId}`,
+      'DELETE',
+      undefined,
+      token
+    )
+  }
 }
 
 export default AccountMembershipsEndpoint
