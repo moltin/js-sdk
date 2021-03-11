f1cd59aec34859b5bffe662dc733cd1ed321a44e
razkevich <razkevich8@gmail.com>
2021-03-11 22:14:44 +0300

    [MT-6474] Added Account Memberships (#421)
    * [MT-6474] Added Account Memberships
    
    * [MT-6474] Added Account Memberships
    
    * [MT-6474] Added Account Memberships
    
    * [MT-6474] Added Account Memberships
    
    * [MT-6474] Added Account Memberships
    
    * [MT-6474] Added Account Memberships
    
    * [MT-6474] Added Account Memberships

---
+++ src/types/account-memberships.d.ts
@@ -0,0 +1,64 @@
+/**
+ * Account Memberships
+ */
+import {Resource,} from './core'
+
+/**
+ * The Account Membership object Interface
+ */
+
+export interface AccountMembership {
+    type: string
+    meta: {
+        timestamps: {
+            created_at: string,
+            updated_at: string
+        }
+    },
+    relationships: {
+        "account-member": {
+            data: {
+                id: string,
+                type: string
+            }
+        }
+    }
+}
+
+export interface AccountMembershipCreateBody {
+    type: string,
+    account_member_id: string
+}
+
+/**
+ * Account Memberships Endpoints
+ */
+export interface AccountMembershipsEndpoint {
+    endpoint: 'account-member'
+    storage: Storage
+
+    /**
+     * Get an Account Membership by reference
+     * @param accountId - The ID for the requested account,
+     * @param accountMembershipId - The ID for the requested account membership,
+     * @param token - The Bearer token to grant access to the API.
+     */
+    Get(accountId: string, accountMembershipId: string, token?: string): Promise<Resource<AccountMembership>>
+
+    /**
+     * Create an Account Membership
+     * @param accountId - The ID for the requested account,
+     * @param body
+     * @param token
+     */
+    Create(accountId: string, body: AccountMembershipCreateBody, token?: string): Promise<Resource<AccountMembership>>
+
+    /**
+     * Delete an Account Membership by reference
+     * @param accountId - The ID for the requested account,
+     * @param accountMembershipId - The ID for the requested account membership,
+     * @param token - The Bearer token to grant access to the API.
+     */
+    Delete(accountId: string, accountMembershipId: string, token?: string): Promise<{}>
+
+}
