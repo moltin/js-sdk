9d9eba72055fd3815ad3110fae9f01da0f03eaa1
Rostyk <rkanafotskyy@gmail.com>
2021-05-12 11:28:35 +0300

    feat: pcm product file relationship

--- src/endpoints/pcm.js
+++ src/endpoints/pcm.js
@@ -1,10 +1,13 @@
 import CRUDExtend from '../extends/crud'
+import PCMFileRelationshipEndpoint from './pcm-file-relationship'
 
 class PCMEndpoint extends CRUDExtend {
   constructor(endpoint) {
     const config = { ...endpoint } // Need to clone config so it is only updated in PCM
     config.version = 'pcm'
     super(config)
+
+    this.FileRelationships = new PCMFileRelationshipEndpoint(config)
 
     this.endpoint = 'products'
   }
---
+++ src/types/pcm-file-relationship.ts
@@ -0,0 +1,59 @@
+/**
+ * Products file relationship
+ */
+import { Identifiable, ResourceList } from "./core";
+
+export interface PcmFileRelationship extends Identifiable {
+  meta: {
+    created_at: string
+  }
+  type: string
+}
+
+export interface PcmFileRelationshipEndpoint {
+  endpoint: 'relationships/files'
+
+  /**
+   * Get all product file relationships
+   * Description:
+   * @param productId
+   * @constructor
+   */
+  All(productId: string): Promise<ResourceList<PcmFileRelationship>>
+
+  /**
+   * Create a product file relationship
+   * Description:
+   * @param productId
+   * @param resources
+   * @constructor
+   */
+  Create(
+    productId: string,
+    resources?: string | string[]
+  ): Promise<void>
+
+  /**
+   * Delete a product file relationship
+   * Description:
+   * @param productId
+   * @param resources
+   * @constructor
+   */
+  Delete(
+    productId: string,
+    resources?: string | string[]
+  ): Promise<void>
+  /**
+   * Update a product file relationship
+   * Description:
+   * @param productId
+   * @param resources
+   * @constructor
+   */
+
+  Update(
+    productId: string,
+    resources?: string | string[]
+  ): Promise<void>
+}
