d4a151b949c80f5dc98450d6c20ef6a7bb3d17c1
Alex Razkevich <razkevich8@gmail.com>
2021-08-17 14:34:33 +0300

    Realms fixed

--- playground/index.ts
+++ playground/index.ts
@@ -1,18 +1,52 @@
-
-import * as moltin from './../';
+import * as moltin from '..'
 
 async function main() {
   const gateway = moltin.gateway({
     host: 'epcc-integration.global.ssl.fastly.net',
-    client_id: 'client_id',
-    client_secret: 'client_secret',
+    client_id: 'R7paB5xa2ngzuEM8N6PkTOGpO9jTcAgW1DO2U2KzOl',
+    client_secret: 'lsKTS0hKMzbxpNBAVjX0NoXt8FeALVPcQxh4lgofWD',
     headers: {
-      'EP-Beta-Features': 'account-management',
-    },
-  });
+      'EP-Beta-Features': 'account-management,accounts'
+    }
+  })
+  // await gateway.Authenticate()
 
-  // const accounts = await gateway.Accounts.All();
-  // console.log(accounts);
+  // await gateway.Accounts.Create({
+  //   legal_name: 'legal1',
+  //   name: 'name1',
+  //   type: 'account',
+  //   registration_id: 'registration_id1'
+  // })
+  // await gateway.Accounts.Create({
+  //   legal_name: 'legal1',
+  //   name: 'name1',
+  //   type: 'account',
+  //   registration_id: 'registration_id1_1'
+  // })
+  // await gateway.Accounts.Create({
+  //   legal_name: 'legal2',
+  //   name: 'name2',
+  //   type: 'account',
+  //   registration_id: 'registration_id2'
+  // })
+  // await gateway.Accounts.Create({
+  //   legal_name: 'legal3',
+  //   name: 'name3',
+  //   type: 'account',
+  //   registration_id: 'registration_id3'
+  // })
+  // await gateway.Accounts.Create({
+  //   legal_name: 'legal4',
+  //   name: 'name4',
+  //   type: 'account',
+  //   registration_id: 'registration_id4'
+  // })
+  console.log('=======account created successfully')
+
+  const accounts = await gateway.Accounts.Filter({
+    like: { name: 'name3' }
+  }).All()
+
+  console.log(accounts)
 }
-
-main().catch(console.error);
+main().catch(console.error)
