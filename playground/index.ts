02fd7f93a458fecca73008c8516f9a85cb313eae
Alex Razkevich <razkevich8@gmail.com>
2021-08-18 14:22:55 +0300

    revert playground

--- playground/index.ts
+++ playground/index.ts
@@ -1,4 +1,5 @@
-import * as moltin from '..'
+
+import * as moltin from './../';
 
 async function main() {
   const gateway = moltin.gateway({
@@ -6,47 +7,12 @@ async function main() {
     client_id: 'client_id',
     client_secret: 'client_secret',
     headers: {
-      'EP-Beta-Features': 'account-management,accounts'
-    }
-  })
-  // await gateway.Authenticate()
+      'EP-Beta-Features': 'account-management',
+    },
+  });
 
-  // await gateway.Accounts.Create({
-  //   legal_name: 'legal1',
-  //   name: 'name1',
-  //   type: 'account',
-  //   registration_id: 'registration_id1'
-  // })
-  // await gateway.Accounts.Create({
-  //   legal_name: 'legal1',
-  //   name: 'name1',
-  //   type: 'account',
-  //   registration_id: 'registration_id1_1'
-  // })
-  // await gateway.Accounts.Create({
-  //   legal_name: 'legal2',
-  //   name: 'name2',
-  //   type: 'account',
-  //   registration_id: 'registration_id2'
-  // })
-  // await gateway.Accounts.Create({
-  //   legal_name: 'legal3',
-  //   name: 'name3',
-  //   type: 'account',
-  //   registration_id: 'registration_id3'
-  // })
-  // await gateway.Accounts.Create({
-  //   legal_name: 'legal4',
-  //   name: 'name4',
-  //   type: 'account',
-  //   registration_id: 'registration_id4'
-  // })
-  console.log('=======account created successfully')
-
-  const accounts = await gateway.Accounts.Filter({
-    like: { name: 'name3' }
-  }).All()
-
-  console.log(accounts)
+  // const accounts = await gateway.Accounts.All();
+  // console.log(accounts);
 }
-main().catch(console.error)
+
+main().catch(console.error);
