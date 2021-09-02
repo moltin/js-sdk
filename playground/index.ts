942c1739deb232b29ccf49d2b28452dc1528f178
Alex Razkevich <razkevich8@gmail.com>
2021-09-02 07:26:36 +0300

    revert playground

--- playground/index.ts
+++ playground/index.ts
@@ -1,10 +1,11 @@
+
 import * as moltin from './../';
 
 async function main() {
   const gateway = moltin.gateway({
     host: 'epcc-integration.global.ssl.fastly.net',
-    client_id: 'ueNTGjzwlYV2LkFbNwsPTWYau5nYEVBCcGIkHDrV9A',
-    client_secret: '0uB0rxJaWXdStn1BmHVcTx8AsmvnLoxWdhJDvUDvj3',
+    client_id: 'client_id',
+    client_secret: 'client_secret',
     headers: {
       'EP-Beta-Features': 'account-management',
     },
@@ -12,8 +13,6 @@ async function main() {
 
   // const accounts = await gateway.Accounts.All();
   // console.log(accounts);
-  const accounts = await gateway.AccountMembers.Filter({eq:{name:'John Doe'}}).All()
-  console.log(accounts)
 }
 
 main().catch(console.error);
