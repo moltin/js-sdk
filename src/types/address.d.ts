f1066dea971f1a5944c07615a3078ace38ed89db
dr-ep <drepgit@gmail.com>
2022-02-07 15:15:04 -0800

    fix(types): Unified Customer add Account addresses

---
+++ src/types/address.d.ts
@@ -0,0 +1,6 @@
+
+import { AccountAddress, AccountAddressBase } from './account-address';
+import { CustomerAddress, CustomerAddressBase } from './customer-address';
+
+export type Address = AccountAddress | CustomerAddress;
+export type AddressBase = AccountAddressBase | CustomerAddressBase;
