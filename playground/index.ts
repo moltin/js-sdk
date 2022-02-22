e2347e8dd529448f343df433689a7c04878381b3
michelle <Michelle Wan>
2022-02-22 14:16:58 -0800

    Reverted playground index changes

--- playground/index.ts
+++ playground/index.ts
@@ -1,33 +1,18 @@
-import * as moltin from '..'
-import nock from 'nock'
-import { rateLimitError } from '../test/factories'
+
+import * as moltin from './../';
 
 async function main() {
   const gateway = moltin.gateway({
     host: 'epcc-integration.global.ssl.fastly.net',
-    client_id: 'LDMeTKqq0VwIkYmqSMZ702hs932VxFw3ScAcYsKMFX',
-    client_secret: 'hvse1weduV0rgUPzmHheEmBo8YQsVjKryY3FJeWle0'
-    // headers: {
-    //   'EP-Beta-Features': 'account-management',
-    // },
-  })
-  const apiUrl = 'https://epcc-integration.global.ssl.fastly.net/v2'
+    client_id: 'client_id',
+    client_secret: 'client_secret',
+    headers: {
+      'EP-Beta-Features': 'account-management',
+    },
+  });
 
-  // // Intercept the API request
-  // nock(apiUrl, {
-  //   reqheaders: {
-  //     Authorization: 'Bearer a550d8cbd4a4627013452359ab69694cd446615a'
-  //   }
-  // })
-  //   .get('/products')
-  //   .reply(429, rateLimitError)
-
-  for (let i = 0; i < 200; i++) {
-    gateway.Products.All()
-  }
-
-  // const products = await gateway.Products.All()
-  // console.log(products)
+  // const accounts = await gateway.Accounts.All();
+  // console.log(accounts);
 }
 
-main().catch(error => console.error(error))
+main().catch(console.error);
