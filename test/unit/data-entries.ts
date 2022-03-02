fef498822f8d1178fe405cdbea14b152ae8405e6
Mahsa ghoreishi <mahsa.ghoreishi@elasticpath.com>
2022-03-02 16:15:11 -0500

    test(test): test fix
    test fix for get all related data entries

--- test/unit/data-entries.ts
+++ test/unit/data-entries.ts
@@ -2,7 +2,7 @@ import { assert } from 'chai'
 import nock from 'nock'
 import { gateway as MoltinGateway } from '../../src/moltin'
 
-const apiUrl = 'https://api.moltin.com'
+const apiUrl = 'https://api.moltin.com/v2'
 
 describe('Moltin Personal Data', () => {
   const Moltin = MoltinGateway({
@@ -11,6 +11,10 @@ describe('Moltin Personal Data', () => {
   //
   it('Get all Related Data Entries', () => {
     nock(apiUrl, {})
+      .post('/oauth/access_token')
+      .reply(200, {
+        access_token: 'a550d8cbd4a4627013452359ab69694cd446615a'
+      })
       .get(
         '/personal-data/related-data-entries?filter=eq(resource_id,64f35045-2a76-4bcf-b6ba-02bb12090d38):eq(resource_type,account)'
       )
