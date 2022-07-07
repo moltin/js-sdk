42c7f0bda9cb6a73f7f6305326407dd46cb72465
butikden <butikxden@gmail.com>
2022-07-07 13:41:17 +0300

    fix: storage usage in config updated to omit useless creation

--- src/config.js
+++ src/config.js
@@ -43,11 +43,11 @@ class Config {
       version,
       language: 'JS'
     }
-    const defaultStorage =
-      storage_type === 'cookies'
+
+    this.storage =
+      storage || storage_type === 'cookies'
         ? new SecureCookiesStorageFactory()
         : new LocalStorageFactory()
-    this.storage = storage || defaultStorage
     this.custom_authenticator = custom_authenticator
     this.headers = headers || {}
     this.disableCart = disableCart || false
