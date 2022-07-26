2a2ad6416fd327759539e9f3b2062bed73b6b577
Mahsa ghoreishi <mahsa.ghoreishi@elasticpath.com>
2022-07-26 13:10:51 -0400

    fix

--- playground/index.ts
+++ playground/index.ts
@@ -3,15 +3,15 @@ import * as moltin from './../';
 async function main() {
   const gateway = moltin.gateway({
     host: 'epcc-integration.global.ssl.fastly.net',
-    client_id: 'DPjTL3UTelnbwTawuEjvTime3KxGtpaPPbxFvkoD60',
-    client_secret: 'wMlK4YvSNpa3SVw3401ADAqmBmyXu1Ed8KEkUIlAsl',
+    client_id: 'client_id',
+    client_secret: 'client_secret',
     headers: {
       'EP-Beta-Features': 'account-management',
     },
   });
 
-  const accounts = await gateway.AccountMembershipSettings.Get();
-  console.log(accounts);
+  // const accounts = await gateway.Accounts.All();
+  // console.log(accounts);
 }
 
 main().catch(console.error);
