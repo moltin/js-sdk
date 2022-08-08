3d082a6c39f4d58e9d9a834c0dfe833c548e02ca
Robert Field <robertfield@quadrimular.com>
2022-08-08 13:13:00 +0100

    fix: missing types for GetAllHierarchies (#688)

--- src/types/hierarchies.d.ts
+++ src/types/hierarchies.d.ts
@@ -2,10 +2,7 @@
  * Products
  * Description: Products are the core resource to any Commerce Cloud project. They can be associated by category, collection, brands, and more.
  */
-import {
-  Identifiable,
-  CrudQueryableResource, ResourceList, ResourcePage
-} from './core'
+import { Identifiable, CrudQueryableResource, ResourcePage } from './core'
 import { NodesEndpoint, Node } from './nodes'
 import { NodeRelationshipsEndpoint } from './node-relationships'
 
@@ -19,6 +16,9 @@ export interface HierarchyBase {
     name: string
     description?: string
     slug?: string
+    created_at?: string
+    updated_at?: string
+    published_at?: string
   }
 }
 
@@ -46,26 +46,32 @@ export interface HierarchyFilter {
 }
 
 type HierarchySort = // TODO
-  | ''
+  ''
 
 type HierarchyInclude = // TODO
-  | ''
+  ''
 
 /**
  * PCM Product Endpoints
  */
 export interface HierarchiesEndpoint
-  extends CrudQueryableResource<Hierarchy,
+  extends CrudQueryableResource<
+    Hierarchy,
     HierarchyBase,
     Partial<HierarchyBase>,
     HierarchyFilter,
     HierarchySort,
-    HierarchyInclude> {
+    HierarchyInclude
+  > {
   endpoint: 'hierarchies'
   Nodes: NodesEndpoint
   Relationships: NodeRelationshipsEndpoint
   Children(id: string, token?: string): Promise<ResourcePage<Node>>
-  Duplicate(hierarchyId: string, body: DuplicateHierarchyBody, token?: string): Promise<Hierarchy>
+  Duplicate(
+    hierarchyId: string,
+    body: DuplicateHierarchyBody,
+    token?: string
+  ): Promise<Hierarchy>
   Limit(value: number): HierarchiesEndpoint
   Offset(value: number): HierarchiesEndpoint
 }
