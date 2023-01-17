708704c34934c940861320a1436c440d08e551f0
Sam Blacklock <sam.blacklock@elasticpath.com>
2023-01-17 10:26:40 +0000

    fix: reindent at 2 spaces

--- src/types/audit-logs.d.ts
+++ src/types/audit-logs.d.ts
@@ -1,30 +1,29 @@
-
 import { Identifiable, ResourcePage } from './core'
 
 export interface AuditLogsRecord extends Identifiable {
-    store_id: string;
-    type: string;
-    initiator: {
-        "access-token-email": string;
-        "access-token-id": string;
-        "access-token-name": string;
-        "access-token-store-id": string;
-        "access-token-type": string;
-    }
-    time: string;
-    event_type: string;
-    delta: Record<string, any>;
-    resource_type: string;
-    relationships: Record<string, any>;
-    links: Record<string, string>;
+  store_id: string
+  type: string
+  initiator: {
+    'access-token-email': string
+    'access-token-id': string
+    'access-token-name': string
+    'access-token-store-id': string
+    'access-token-type': string
+  }
+  time: string
+  event_type: string
+  delta: Record<string, any>
+  resource_type: string
+  relationships: Record<string, any>
+  links: Record<string, string>
 }
 
 export interface AuditLogsEndpoint {
-    All(): Promise<ResourcePage<AuditLogsRecord>>
+  All(): Promise<ResourcePage<AuditLogsRecord>>
 
-    Filter(resourceType: string, resourceId: string): AuditLogsEndpoint
+  Filter(resourceType: string, resourceId: string): AuditLogsEndpoint
 
-    Limit(value: number): AuditLogsEndpoint
+  Limit(value: number): AuditLogsEndpoint
 
-    Offset(value: number): AuditLogsEndpoint
+  Offset(value: number): AuditLogsEndpoint
 }
