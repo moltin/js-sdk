6c21fd7b8b8caabc8eb621b5d109ac251c4a51a7
krish4uu <kashyap-prajapati@pluralsight.com>
2023-01-18 17:27:49 -0500

    Updated test for errors

--- test/integration/throttle.ts
+++ test/integration/throttle.ts
@@ -4,45 +4,79 @@ import nock from 'nock'
 import { assert } from 'chai'
 
 const apiUrl = 'https://api.moltin.com/v2'
-const client = gateway({
-  client_id: 'XXX'
-})
 
 let now = Date.now()
 
 console.log(`Start: ${now}`)
 
-const throttle = throttledQueue(3, 125)
-
-for (let index = 1; index <= 500; index++) {
-  const msecDiff = Date.now() - now
-  throttle(async () => {
-
-    nock(apiUrl, {
-      reqheaders: {
-        Authorization: 'Bearer a550d8cbd4a4627013452359ab69694cd446615a'
-      }
-    })
-      .get(
-        `/pcm/products?filter=eq(slug,
-      ${index % 100}product-slug-1670366469995)`
-      )
-      .reply(200, { data: { meta: { results: {total: 0 } }} })
-
-    return client.PCM.Filter({
-      eq: {
-        slug: `${index % 100}product-slug-1670366469995`
-      }
-    })
-      .All()
-      .then(response => {
-        console.log(response.meta.results)
-        console.log(`${index}: ${msecDiff}`)
-        assert.equal(response.meta.results, {total: 0 })
-      })
-      .catch(err => {
-        console.error('error', err)
-        assert.equal(err, { errors: [ { title: 'Rate limit was reached', status: 429 } ] })
-      })
+describe('Moltin addresses', () => {
+  const moltin = gateway({
+    client_id: 'XXX',
+    client_secret: 'XXX'
   })
-}
+
+  it('should return successful throttling', () => {
+    let now = Date.now()
+
+    console.log(`Start: ${now}`)
+
+    const throttle = throttledQueue(3, 125)
+
+    for (let index = 1; index <= 500; index++) {
+      const msecDiff = Date.now() - now
+      // Intercept the API request
+      nock(apiUrl, {
+        reqheaders: {
+          Authorization: 'Bearer 902cf5f0d01a1d65067df82c7305a1a636220fc4'
+        }
+      })
+        .persist()
+        .get(`/carts/:${index}`)
+        .times(500)
+        .reply(200, { data: { id: `${index}` } })
+      throttle(async () => {
+        return moltin
+          .Cart(`${index}`)
+          .Get()
+          .then(response => {
+            console.log(`CartId: ${response.data.id}`)
+            console.log(`${index}: ${msecDiff}`)
+            assert.equal(response.data.id, `${index}`)
+          })
+      })
+    }
+  })
+
+  it('should return error for throttling', () => {
+    let now = Date.now()
+
+    console.log(`Start: ${now}`)
+
+    const throttle = throttledQueue(3, 125)
+
+    for (let index = 1; index <= 500; index++) {
+      const msecDiff = Date.now() - now
+      // Intercept the API request
+      nock(apiUrl, {
+        reqheaders: {
+          Authorization: 'Bearer 902cf5f0d01a1d65067df82c7305a1a636220fc4'
+        }
+      })
+        .persist()
+        .get(`/carts/:${index}`)
+        .times(500)
+        .replyWithError({ title: 'Rate limit was reached', status: 429 })
+      throttle(async () => {
+        return moltin
+          .Cart(`${index}`)
+          .Get()
+          .catch(err => {
+            console.error('error', err)
+            assert.equal(err, {
+              errors: [{ title: 'Rate limit was reached', status: 429 }]
+            })
+          })
+      })
+    }
+  })
+})
