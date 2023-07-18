8f731e470cb53b9d29e91f0937aba8d6a4b510ee
Rostyk <34774987+rostyk-kanafotskyy@users.noreply.github.com>
2023-07-18 18:11:52 +0300

    feat: offering plans (#832)

--- src/types/subscription-offerings.d.ts
+++ src/types/subscription-offerings.d.ts
@@ -44,12 +44,33 @@ export interface SubscriptionOfferingBuildBody {
   plans: string[]
 }
 
+export interface SubscriptionOfferingAttachProductBody {
+  products: string[]
+}
+
+export interface SubscriptionOfferingAttachPlanBody {
+  plans: string[]
+}
+
 export interface SubscriptionOffering extends Identifiable, SubscriptionOfferingBase {
 
 }
 export type SubscriptionOfferingCreate = Omit<SubscriptionOfferingBase, 'attributes'> & {attributes: Partial<SubscriptionOfferingBase['attributes']>}
 export type SubscriptionOfferingUpdate = Omit<SubscriptionOffering, 'attributes'> & {attributes: Partial<SubscriptionOfferingBase['attributes']>}
 
+type SubscriptionOfferingAttachmentsRelationships = {
+  relationships: {
+    origin: {
+      data: {
+        id: string,
+        type: string
+      }
+    }
+  }
+}
+
+export type SubscriptionOfferingPlan = SubscriptionPlan & SubscriptionOfferingAttachmentsRelationships
+export type SubscriptionOfferingProduct = SubscriptionProduct & SubscriptionOfferingAttachmentsRelationships
 /**
  * Subscription Offering Endpoints
  * DOCS: TODO: add docs when ready
@@ -67,7 +88,15 @@ export interface SubscriptionOfferingsEndpoint
 
   Build(body: SubscriptionOfferingBuildBody): Promise<Resource<SubscriptionOffering>>
 
-  GetAttachedProducts(id: string) : Promise<Resource<SubscriptionProduct[]>>
+  GetAttachedProducts(id: string) : Promise<Resource<SubscriptionOfferingProduct[]>>
 
-  GetAttachedPlans(id: string) : Promise<Resource<SubscriptionPlan[]>>
+  GetAttachedPlans(id: string) : Promise<Resource<SubscriptionOfferingPlan[]>>
+
+  AttachProducts(offeringId: string, body: SubscriptionOfferingAttachProductBody): Promise<Resource<SubscriptionProduct[]>>
+
+  RemoveProduct(offeringId: string, productId: string): Promise<void>
+
+  AttachPlans(offeringId: string, body: SubscriptionOfferingAttachPlanBody): Promise<Resource<SubscriptionPlan[]>>
+
+  RemovePlan(offeringId: string, planId: string): Promise<void>
 }
