f90c91b1dba42dd87c56e4c659b5867af9a48b23
Michelle Wan <michelle.wan@elasticpath.com>
2023-08-11 11:35:17 -0700

    Remove comments and console.log

--- src/endpoints/one-time-password-token-request.js
+++ src/endpoints/one-time-password-token-request.js
@@ -4,15 +4,10 @@ class OneTimePasswordTokenRequestEndpoint extends CRUDExtend {
   constructor(config) {
     super(config)
     this.endpoint =
-      // 'authentication-realms/{{realmId}}/password-profiles/{{passwordProfileId}}/one-time-password-token-request'
       'authentication-realms/{{realmId}}/password-profiles/{{passwordProfileId}}/one-time-password-token-request'
   }
 
   Create(realmId, passwordProfileId, body, token = null) {
-    console.log('test', this.endpoint
-      .replace('{{realmId}}', realmId)
-      .replace('{{passwordProfileId}}', passwordProfileId))
-    console.log(body)
     return this.request.send(
       this.endpoint
         .replace('{{realmId}}', realmId)
