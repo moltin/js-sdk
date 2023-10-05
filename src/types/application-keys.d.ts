2dd47b0dd7eb7adf216d236282377ed14c57d1db
Dusan Radovanovic <dusan.radovanovic@elasticpath.com>
2023-10-04 18:19:02 -0700

    feat(types): added signatures for application keys rps limit

--- src/types/application-keys.d.ts
+++ src/types/application-keys.d.ts
@@ -9,12 +9,19 @@ export interface ApplicationKey extends ApplicationKeyBase, Identifiable {
     id: string
     client_id: string
     client_secret?: string
+    reserved_rate_limit: number | null
     meta: {
       timestamps: {
         created_at: string
         updated_at: string
       }
     }
+}
+
+interface MetaReservedRps {
+  meta: {
+    total_reserved_rate_limit: number
+  }
 }
 
 export interface ApplicationKeyResponse extends Resource<ApplicationKey> {
@@ -31,7 +38,7 @@ export interface ApplicationKeysEndpoint extends CrudQueryableResource<
   never,
   never
 > {
-  All(): Promise<ResourcePage<ApplicationKey>>
+  All(): Promise<ResourcePage<ApplicationKey> & MetaReservedRps>
   Create(body: ApplicationKeyBase): Promise<ApplicationKeyResponse>
   Delete(id: string): Promise<{}>
 }
