58b715c4982640f225efb8ae6e560ec843bbee79
Rostyk <34774987+rostyk-kanafotskyy@users.noreply.github.com>
2024-06-20 14:21:52 +0300

    feat: subscription proration policies endpoint (#936)
    * feat: subscription proration policies endpoint
    
    * fix: removed missing fields

---
+++ src/types/subscription-proration-policies.d.ts
@@ -0,0 +1,52 @@
+/**
+ * Subscription Proration Policies
+ * Description: Subscription Proration Policies.
+ * DOCS: TODO: add docs when ready
+ */
+import {
+  Identifiable,
+  CrudQueryableResource
+} from './core'
+
+/**
+ * Core Subscription Proration Policies Base Interface
+ * For custom flows, extend this interface
+ * DOCS: TODO: add docs when ready
+ */
+export interface SubscriptionProrationPolicyBase {
+  type: 'proration_policy'
+  attributes: {
+    name: string
+    rounding: 'up' | 'down' | 'nearest'
+    external_ref?: string
+  }
+}
+
+export interface SubscriptionProrationPolicy extends Identifiable, SubscriptionProrationPolicyBase {
+  meta: {
+    owner: 'store' | 'organization'
+    timestamps: {
+      updated_at: string
+      created_at: string
+    }
+  }
+}
+
+export type SubscriptionProrationPolicyCreate = SubscriptionProrationPolicyBase
+export type SubscriptionProrationPolicyUpdate = Identifiable & Omit<SubscriptionProrationPolicyBase, 'attributes'> & {attributes: Partial<SubscriptionProrationPolicy['attributes']>}
+
+/**
+ * Subscription Proration Policies Endpoints
+ * DOCS: TODO: add docs when ready
+ */
+export interface SubscriptionProrationPoliciesEndpoint
+  extends CrudQueryableResource<
+    SubscriptionProrationPolicy,
+    SubscriptionProrationPolicyCreate,
+    SubscriptionProrationPolicyUpdate,
+    never,
+    never,
+    never
+    > {
+  endpoint: 'proration-policies'
+}
